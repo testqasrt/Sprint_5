d317f8b69634521dbb15c9584d7b23c841e290c8
alexandr <yakhlakovalexandr@gmail.com>
2024-08-18 18:45:03 +0300

    объеденил тесты в класс

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -11,22 +11,23 @@ from urls import (BASE_URL, LOGIN_PAGE_URL, RECOVERY_PASSWORD_PAGE,
                   REGISTRATION_PAGE_URL)
 
 
-@pytest.mark.parametrize('url, button',
-                         ((BASE_URL, (By.XPATH, main_page_login_button,)),
-                          (REGISTRATION_PAGE_URL, (By.CSS_SELECTOR, reg_page_login_button)),
-                          (RECOVERY_PASSWORD_PAGE, (By.CSS_SELECTOR, reg_page_login_button)),
-                          (BASE_URL, (By.CSS_SELECTOR, personal_cabinet_button))))
-def test_successful_login_from_pages(driver, fill_out_auth_form, url, button):
-    user = RegisteredUser()
-    driver.get(url)
-    driver.find_element(*button).click()
-    WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, login_form_header)))
-    assert driver.current_url == LOGIN_PAGE_URL
+class TestAuth:
+    @pytest.mark.parametrize('url, button',
+                             ((BASE_URL, (By.XPATH, main_page_login_button,)),
+                              (REGISTRATION_PAGE_URL, (By.CSS_SELECTOR, reg_page_login_button)),
+                              (RECOVERY_PASSWORD_PAGE, (By.CSS_SELECTOR, reg_page_login_button)),
+                              (BASE_URL, (By.CSS_SELECTOR, personal_cabinet_button))))
+    def test_successful_login_from_pages(self, driver, fill_out_auth_form, url, button):
+        user = RegisteredUser()
+        driver.get(url)
+        driver.find_element(*button).click()
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, login_form_header)))
+        assert driver.current_url == LOGIN_PAGE_URL
 
-    fill_out_auth_form(driver, user.email, user.password)
-    WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH,
-                                                                                      main_page_order_create_button)))
-    assert driver.find_element(By.XPATH, main_page_order_create_button).is_displayed()
-    assert driver.current_url == BASE_URL
+        fill_out_auth_form(driver, user.email, user.password)
+        WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH,
+                                                                                          main_page_order_create_button)))
+        assert driver.find_element(self, By.XPATH, main_page_order_create_button).is_displayed()
+        assert driver.current_url == BASE_URL
 
 
